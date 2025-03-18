13d92891ecd9958ec8503adda485e4d3b56c8a93
Fort163 <Front-91@mail.ru>
2025-03-18 23:26:30 +0400

    add migration message

---
+++ src/main/resources/db/changelog/script/create-schema-notification-message.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS notification_message
+(
+    uuid         uuid primary key,
+    created_by   varchar(255),
+    created_when timestamp,
+    updated_by   varchar(255),
+    updated_when timestamp,
+    is_active    boolean,
+    from_user    varchar(255),
+    to_user      varchar(255),
+    send_type   varchar(100) NOT NULL,
+    message_type   varchar(100) NOT NULL,
+    project      varchar(100) NOT NULL,
+    message      text,
+    json_object      text,
+    path         varchar(255),
+    message_path     varchar(255),
+    send         boolean,
+    received     boolean
+);
