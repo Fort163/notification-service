d9ae75d16e209a2d9e917d761c59caa9e926a84e
Fort163 <Front-91@mail.ru>
2025-03-31 03:29:13 +0400

    fix change package

--- src/main/java/com/quick/recording/notification/service/service/mail/NotificationMailService.java
+++ src/main/java/com/quick/recording/notification/service/service/mail/NotificationMailService.java
@@ -1,7 +1,7 @@
 package com.quick.recording.notification.service.service.mail;
 
 import com.quick.recording.gateway.dto.notification.mail.NotificationMailDto;
-import com.quick.recording.gateway.main.service.MainService;
+import com.quick.recording.gateway.main.service.local.MainService;
 import com.quick.recording.notification.service.entity.NotificationMailEntity;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/quick/recording/notification/service/service/mail/NotificationMessageService.java
+++ src/main/java/com/quick/recording/notification/service/service/mail/NotificationMessageService.java
@@ -1,7 +1,7 @@
 package com.quick.recording.notification.service.service.mail;
 
 import com.quick.recording.gateway.dto.notification.message.NotificationMessageDto;
-import com.quick.recording.gateway.main.service.MainService;
+import com.quick.recording.gateway.main.service.local.MainService;
 import com.quick.recording.notification.service.entity.NotificationMessageEntity;
 import org.springframework.stereotype.Service;
 
