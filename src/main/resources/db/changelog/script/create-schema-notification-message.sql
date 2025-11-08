ddb31af128a8fb0f4060bba404d4f8243e4489fd
Fort163 <Front-91@mail.ru>
2025-11-08 16:40:30 +0400

    fix schema message

--- src/main/resources/db/changelog/script/create-schema-notification-message.sql
+++ src/main/resources/db/changelog/script/create-schema-notification-message.sql
@@ -5,17 +5,17 @@ CREATE TABLE IF NOT EXISTS notification_message
     created_when timestamp,
     updated_by   varchar(255),
     updated_when timestamp,
-    is_active    boolean,
-    from_user    varchar(255),
-    to_user      varchar(255),
+    is_active    boolean NOT NULL,
+    from_user    varchar(255) NOT NULL,
+    to_user      varchar(255) NOT NULL,
     send_type   varchar(100) NOT NULL,
     message_type   varchar(100) NOT NULL,
     project      varchar(100) NOT NULL,
-    message      text NOT NULL,
+    message      text,
     message_code     varchar(255),
     json_object      text,
     path         varchar(255),
     message_path     varchar(255),
-    send         boolean,
-    received     boolean
+    send         boolean NOT NULL,
+    received     boolean NOT NULL
 );
